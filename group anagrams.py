9ea6d89ed3fd64fc4f8e64f52843b2ccc3417d01
Jacksonjse <jacksonjess2205@gmail.com>
2025-09-10 12:42:35 +0530

    group anagrams solve, check notion for notes

--- group anagrams.py
+++ group anagrams.py
@@ -1,16 +1,12 @@
-class Solution(object):
-    #using a hashmap(defaultdict(list) -- stores the values in a list)
-    #each character in a string is counted and its character count is listed out as an list with 26 elements
-    #each of the alphabet
-    #teh counts are used as key to store the strings that have the same number of those characters as values in the defaultdict
-    
-    def groupAnagrams(self, strs):
-        res = defaultdict(list)
-        for loop in strs:
-            count = [0] * 26
-            for i in loop:
-                char = ord(i) - ord("a")
-                count[char] +=1
-            res[tuple(count)].append(loop)
-        return res.values()
+from collections import defaultdict
 
+def group_anagrams(str):
+    hashmap = defaultdict(list)
+    for i in str:
+        char = [0] * 26
+        for j in i:
+            list_place = ord(j) - ord('a')
+            char[list_place] += 1
+        hashmap[tuple(char)].append(i)
+    print(hashmap.values())
+group_anagrams(["eat","tea","tan","ate","nat","bat"])
