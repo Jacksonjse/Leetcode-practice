d48422dc3f343e2bdc652a2fbaa70a1e76bdcbf5
Jackson Jesse <jacksonjess2205@gmail.com>
2026-01-06 22:20:00 +0530

    in the pursuit of greatness - DAY 1

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+          Set<Integer> hs= new HashSet<>();
+          for(int i=0; i<nums.length; i++){
+            if (hs.contains(nums[i])) return true;
+            else hs.add(nums[i]);
+          }
+        return false;
+    }
+}
