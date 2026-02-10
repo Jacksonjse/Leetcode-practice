deeb64bc9bead1988e9b0665fe92a211b5b148fb
Jacksonjse <jacksonjess2205@gmail.com>
2026-02-10 21:45:25 +0530

    leetcode feb - 10

--- 242. Valid Anagram.java
+++ 242. Valid Anagram.java
@@ -1,18 +1,15 @@
-import java.util.*;
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        else{
-            char[] sChar = s.toCharArray();
-            char[] tChar = t.toCharArray();
-
-            Arrays.sort(sChar);
-            Arrays.sort(tChar);
-
-            return Arrays.equals(sChar, tChar);
-            }
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
         
-    }
-}
+        countS_T = {}
+
+        for i in range(len(s)):
+            countS_T[s[i]] = 1 + countS_T.get(s[i], 0)
+            countS_T[t[i]] = countS_T.get(t[i], 0) - 1
+
+        for c in countS_T:
+            if countS_T[c] != 0:
+                return False
+        return True
